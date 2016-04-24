2e0a434ecf0f329b061a05a5508cf05f57dfc737
Josh Kalov <jkalov@gmail.com>
2016-04-24 15:29:23 -0500

    build json output for statuses

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -5,6 +5,50 @@ class StatusesController < ApplicationController
   # GET /statuses.json
   def index
     @statuses = Status.all
+
+    respond_to do |format|
+      format.html do
+        authenticate_user!
+        if !user_signed_in? || (!current_user.admin && current_user.organization.nil?)
+          sign_out :user
+          redirect_to statuses_path
+          return  
+        end
+        @statuses = Status.all
+      end
+      format.json do
+          @statuses = Status.all
+        # if params[:source_id].nil?
+        #   @trails = cached_all_by_name
+        # else
+        #   @trails = Trail.includes([:photorecord]).where(source_id: params[:source_id]).order("name")
+        # end
+
+        #if stale?(@trails)
+        entity_factory = ::RGeo::GeoJSON::EntityFactory.instance
+        features = []
+        @statuses.each do |status|
+          # taking a trip to Null Island, because RGeo::GeoJSON chokes on empty geometry here
+          json_attributes = create_json_attributes(status)
+          feature = entity_factory.feature(RGeo::Geographic.spherical_factory.point(0,0), status.id, json_attributes)
+          features.push(feature)
+        end
+        collection = entity_factory.feature_collection(features)
+        my_geojson = RGeo::GeoJSON::encode(collection)
+        ojDump = Oj.dump(my_geojson)
+        #if stale?(ojDump)
+          render json: ojDump
+        #end
+      end
+      format.csv do
+        #if params[:source_id].nil?
+          @statuses = Status.all
+        #else
+        #  @trails = Trail.includes([:photorecord]).where(source_id: params[:source_id]).order("name")
+        #end
+        render text: @statuses.to_csv
+      end
+    end
   end
 
   # GET /statuses/1
@@ -61,6 +105,32 @@ class StatusesController < ApplicationController
     end
   end
 
+  def create_json_attributes(status)
+    json_attributes = status.attributes.clone.except!("created_at", "updated_at")
+    # if status.source
+    #   json_attributes["source"] = trail.source.code
+    #   json_attributes["source_fullname"] = trail.source.full_name
+    #   json_attributes["source_phone"] = trail.source.phone
+    #   json_attributes["source_url"] = trail.source.url
+    #   json_attributes["source_logo_url"] = trail.source.logo.url(:medium)
+    # end
+    # if trail.steward 
+    #   json_attributes["steward"] = trail.steward.code
+    #   json_attributes["steward_fullname"] = trail.steward.full_name
+    #   json_attributes["steward_phone"] = trail.steward.phone
+    #   json_attributes["steward_url"] = trail.steward.url
+    #   json_attributes["steward_logo_url"] = trail.steward.logo.url(:medium)
+    # end
+    # if trail.photorecord
+    #   json_attributes["orig_photo_url"] = trail.photorecord.photo.url
+    #   json_attributes["medium_photo_url"] = trail.photorecord.photo.url(:medium)
+    #   json_attributes["thumb_photo_url"] = trail.photorecord.photo.url(:thumb)
+    #   json_attributes["photo_credit"] = trail.photorecord.credit
+    # end
+    #json_attributes["id"] = trail.trail_id
+    json_attributes
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_status
@@ -71,4 +141,4 @@ class StatusesController < ApplicationController
     def status_params
       params.require(:status).permit(:status_type, :reason, :title, :description, :start_date, :end_date, :statusable_type, :statusable_id, :created_by)
     end
-end
+  end
