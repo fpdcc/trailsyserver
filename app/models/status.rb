c8b2a7d0060097308d9eb5e9b1cd6faa35459d90
Josh Kalov <jkalov@gmail.com>
2016-04-24 14:19:33 -0500

    statuses can be for entrance/POI or Trail

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,6 +1,9 @@
 class Status < ActiveRecord::Base
 
 	validates :start_date, presence: true
+	validates :statusable_type, presence:true
+	#enum      statusable_type: { entrance: 0, trail: 1 }
+	validates :statusable_id, presence:true
 	validates :status_type,       presence: true
 	enum      status_type: { alert: 0, closure: 1 }
 
