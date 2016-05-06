e8389f158f209a3602e4fabba7d9fbd2a5326ad1
Josh Kalov <jkalov@gmail.com>
2016-05-06 14:18:43 -0500

    basic alertings validations

--- app/models/alerting.rb
+++ app/models/alerting.rb
@@ -1,6 +1,16 @@
 class Alerting < ActiveRecord::Base
 
 	belongs_to :alert
+
+	# Yes, but we want higher level users to be able to create alert when creating alerting
+	validates  :alert, presence: true
+
   	belongs_to :alertable, :polymorphic => true
+  	validates  :alertable, presence: true
+
+  	validates :alertable_type, presence: true
+  	validates :alertable_id, presence: true
+  	validates :alert_id, presence: true
+  	validates :starts_at, presence: true
 
 end
