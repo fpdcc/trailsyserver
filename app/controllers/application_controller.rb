29b24e292ddee6b3b43f23337d7a15f93f8056bd
Josh Kalov <jkalov@gmail.com>
2016-11-09 15:17:51 -0600

    switch devise .for to .permit since .for is deprecated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :organization_id
+  	devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :organization_id])
+    #devise_parameter_sanitizer.for(:sign_up) << :organization_id
   end
 end
