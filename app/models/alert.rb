243848ab1dd2ea028c0ca5036c2eeccc5c2e5dd4
Josh Kalov <jkalov@gmail.com>
2016-11-11 13:58:14 -0600

    change alerts connection to pointsofinterests

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -3,7 +3,7 @@ class Alert < ActiveRecord::Base
 	has_many :alertings
   	has_many :new_trails, :through => :alertings, :source => :alertable,
     	:source_type => 'Trail'
-  	has_many :poi_infos, :through => :alertings, :source => :alertable,
+  	has_many :pointsofinterests, :through => :alertings, :source => :alertable,
     	:source_type => 'POI'
 
     validates :alert_type,       presence: true
