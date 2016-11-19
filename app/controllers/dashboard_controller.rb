e4bb2a3a1cef3db941f9469a78fb7ef0c8a9fb59
Josh Kalov <jkalov@gmail.com>
2016-11-19 14:54:04 -0600

    add basic search for alert dashboard poi. Issue #13

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,12 @@
 class DashboardController < ApplicationController
   def poi
   	@pointsofinterests_active = Pointsofinterest.with_current_or_future_alerts #.order(self.active_alerts_count)
-  	@pointsofinterests = Pointsofinterest.all.paginate(page: params[:page])
+  	
+    query = ""
+    if params[:name].present?
+      query = "name ILIKE ?", "#{params[:name]}%"
+    end
+    @pointsofinterests = Pointsofinterest.paginate(page: params[:page]).includes(:alertings).where(query)
   	# @new_alertings = []
     # @pointsofinterests.length.times do
     #   @new_alertings << Alerting.new
