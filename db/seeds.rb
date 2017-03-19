8ac7ce8264188435e694cc52bfb3fda80860ad23
Josh Kalov <jkalov@gmail.com>
2017-03-19 15:44:24 -0500

    create example users for each user level

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,11 @@
 
 ADMIN_USER = (ENV["DEFAULT_ADMIN_USER"] || "admin@example.com").freeze
 PASSWORD   = (ENV["DEFAULT_ADMIN_PASSWORD"] || "password").freeze
-#MPSSC_USER = (ENV["DEFAULT_MPSSC_USER"] || "mpssc@example.com").freeze
+LEVEL1_USER = (ENV["DEFAULT_LEVEL1_USER"] || "level1@example.com").freeze
+LEVEL2_USER = (ENV["DEFAULT_LEVEL2_USER"] || "level2@example.com").freeze
+LEVELUNKNOWN_USER = (ENV["DEFAULT_LEVELUNKNOWN_USER"] || "levelUnknown@example.com").freeze
+
+
 #NPS_USER   = (ENV["TEST_NPS_USER"] || "nps@example.com").freeze
 
 fpdcc = Organization.create({
@@ -27,4 +31,34 @@ User.create({
   password: PASSWORD.dup,
   password_confirmation: PASSWORD.dup,
   role: "admin"
+  })
+
+level1 = User.find_by(email: LEVEL1_USER.dup)
+level1.destroy unless level1.nil?
+User.create({ 
+  email: LEVEL1_USER.dup,
+  approved: true,
+  password: PASSWORD.dup,
+  password_confirmation: PASSWORD.dup,
+  role: "level1"
+  })
+
+level2 = User.find_by(email: LEVEL2_USER.dup)
+level2.destroy unless level2.nil?
+User.create({ 
+  email: LEVEL2_USER.dup,
+  approved: true,
+  password: PASSWORD.dup,
+  password_confirmation: PASSWORD.dup,
+  role: "level2"
+  })
+
+levelUnknown = User.find_by(email: LEVELUNKNOWN_USER.dup)
+levelUnknown.destroy unless levelUnknown.nil?
+User.create({ 
+  email: LEVELUNKNOWN_USER.dup,
+  approved: true,
+  password: PASSWORD.dup,
+  password_confirmation: PASSWORD.dup,
+  role: "unknown"
   })
