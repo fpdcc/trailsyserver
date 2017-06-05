61033b78b6ac23a6055e459496d6e28eedf734fb
Josh Kalov <jkalov@gmail.com>
2017-06-05 17:52:33 -0500

    full opacity if level1 can edit

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -110,9 +110,9 @@ class AlertsController < ApplicationController
   end
 
   def poi
-    @act = Pointsofinterest.has_parking.with_current_or_future_alerts.ransack(params[:q]) #.order(self.active_alerts_count)
+    @act = current_user.level1? ? Pointsofinterest.has_parking.with_current_or_future_alerts.ransack(params[:q]) : Pointsofinterest.with_current_or_future_alerts.ransack(params[:q])
     query = ""
-    @q = Pointsofinterest.has_parking.no_current_or_future_alerts.ransack(params[:q])
+    @q = current_user.level1? ? Pointsofinterest.has_parking.no_current_or_future_alerts.ransack(params[:q]) : Pointsofinterest.no_current_or_future_alerts.ransack(params[:q])
     #@q.sorts = ['poi_info_id asc'] #if @q.sorts.empty?
     @pointsofinterests_active = @act.result.includes(:alerts)
     @pointsofinterests = @q.result.paginate(page: params[:page])
