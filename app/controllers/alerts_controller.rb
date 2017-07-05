1cf92bdb0c16b71670647cc779bc8444450f5707
Josh Kalov <jkalov@gmail.com>
2017-07-05 16:00:19 -0500

    don't require auth for alerts json

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -1,6 +1,6 @@
 class AlertsController < ApplicationController
   before_action :set_alert, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index]
 
   # GET /alerts
   # GET /alerts.json
--- config/application.rb
+++ config/application.rb
@@ -57,10 +57,16 @@ module Trailsyserver
     #     resource '*', :headers => :any, :methods => [:get, :post, :options]
     #   end
     # end
-    config.action_dispatch.default_headers = {
-      'Access-Control-Allow-Origin' => '*',
-      'Access-Control-Request-Method' => %w{GET OPTIONS}.join(",")
-    }
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :options]
+      end
+    end
+    # config.action_dispatch.default_headers = {
+    #   'Access-Control-Allow-Origin' => '*',
+    #   'Access-Control-Request-Method' => %w{GET OPTIONS}.join(",")
+    # }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
