f38c8389e4ddf215abbbb67e5772cecc66a268d8
Josh Kalov <jkalov@gmail.com>
2017-10-25 22:03:58 -0500

    2.4.2

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@ set :branch, ENV['PRODUCTION_BRANCH']
 
 set :rails_env, :production
 set :deploy_to, "/var/www/#{fetch(:application)}"
-
+set :rvm1_ruby_version, "2.4.2"
 set :unicorn_pid, "/var/www/#{fetch(:application)}/shared/tmp/pids/unicorn-#{fetch(:rails_env)}.pid"
 set :unicorn_config_path, "/var/www/#{fetch(:application)}/shared/config/unicorn.rb"
 
