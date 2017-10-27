268c94cd4c0f2fca994f231b1c26e1e8ec360787
Josh Kalov <jkalov@gmail.com>
2017-10-27 13:30:19 -0500

    fix redirect after password update

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,4 +4,8 @@ class RegistrationsController < Devise::RegistrationsController
   def after_inactive_sign_up_path_for(resource)
     trails_path
   end
+
+  def after_update_path_for(resource)
+      poi_alerts_path
+    end
 end
