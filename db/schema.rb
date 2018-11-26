536197f6e4c53a828c109f6e5a27151e24410315
Josh Kalov <jkalov@gmail.com>
2018-11-26 16:59:13 -0600

    merge cleanup

--- db/schema.rb
+++ db/schema.rb
@@ -58,6 +58,21 @@ ActiveRecord::Schema.define(version: 20171028013935) do
     t.integer "origin_type"
   end
 
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer "priority", default: 0, null: false
+    t.integer "attempts", default: 0, null: false
+    t.text "handler", null: false
+    t.text "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string "locked_by"
+    t.string "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
+  end
+
   create_table "new_trails", id: :serial, force: :cascade do |t|
     t.integer "trails_id"
     t.geography "geom", limit: {:srid=>4326, :type=>"line_string", :geographic=>true}
