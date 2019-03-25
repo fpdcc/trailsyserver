585967b849760792e684ea9bac60a648fbc19740
Josh Kalov <jkalov@gmail.com>
2019-03-25 12:15:45 -0500

    staged not success

--- app/jobs/import_file_job.rb
+++ app/jobs/import_file_job.rb
@@ -4,7 +4,7 @@ class ImportFileJob < ApplicationJob
   after_perform do |job|
     logger.info "job arguments = #{job.arguments}"
     update = job.arguments[2]
-    update.status = "success"
+    update.status = "staged"
     #update.job = job
     update.save
     
